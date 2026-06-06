145d182e9001b46b1618ad092aa7a2c78857df8f
Lakshita Vij <lakshitavij@live.com>
2026-06-06 12:06:32 -0400

    creating logic for retrying api calls

---
+++ scaffolding/rate_limiter.py
@@ -0,0 +1,9 @@
+import time
+
+def retry_with_backoff(func, max_retries=3):
+    for attempt in range(max_retries):
+        try:
+            return func()
+        except Exception as e:
+            time.sleep(2**attempt)
+    raise Exception(f"Failed after {max_retries} attempts:{e}")
