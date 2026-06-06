45839d9b3d21ef33055e362deed2003a576ef325
Lakshita Vij <lakshitavij@live.com>
2026-06-06 11:56:55 -0400

    logging errors for our calls

---
+++ scaffolding/observability.py
@@ -0,0 +1,18 @@
+import json
+import datetime
+import os
+os.makedirs('logs', exist_ok=True)
+
+
+def log_tool_call(tool_name, inputs, result, duration, status, timestamp):
+    os.makedirs('logs', exist_ok=True)
+    entry= {
+        "tool name": tool_name,
+        "inputs": inputs,
+        "result": result,
+        "timestamp" : datetime.datetime.now().isoformat(),
+        "duration": duration,
+        "status": status
+    }
+    with open('logs/agent.log', 'a') as f:
+        f.write(json.dumps(entry)+ '\n')
